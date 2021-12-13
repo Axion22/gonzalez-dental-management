6b6a0106ef1bdbb1262899df91399fb81ebc70b9
Axion Ultra <axionultra@gmail.com>
2021-12-13 19:38:07 +0800

    Heroku 4

--- management/context_processors.py
+++ management/context_processors.py
@@ -2,11 +2,9 @@ from website.models import BookAppointment, VirtualConsult
 
 
 def get_notification(request):
-    count = BookAppointment.objects.filter(assign_doctor=None).count()
     countVC = VirtualConsult.objects.filter(assign_doctor=None).count()
        
     data = {
-        "count":count,
         "countVC":countVC,
 
     }
